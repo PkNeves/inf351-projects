ed60c1155c9078a30072b0502d9a1aedd18af611
Petterson Neves <pettersonkesler@gmail.com>
2022-12-26 18:53:49 -0300

    refactor: colocar codigo certo do led gradual

--- tinkercad_ultrasonico_led_gradual/tinkercad_ultrasonico_led_gradual.ino
+++ tinkercad_ultrasonico_led_gradual/tinkercad_ultrasonico_led_gradual.ino
@@ -1,113 +1,90 @@
-// Link projeto: https://www.tinkercad.com/things/fAI9ovxNcrw-frantic-tumelo/editel?sharecode=pYKlDVMLFdy8B9F-QoZ78CVEZKsYuIm2f86PK23kT5A
-
-int trig = 3;
+int trig = 4;
 int echo = 5;
+#define S 6
+#define R 9
+#define B 10
+#define G 11
 
-#define S 4
-#define G1 6
-#define G2 7
-#define Y1 10
-#define Y2 11
-#define R1 12
-#define R2 13
+float distancia;
 
-
-float distancia;	
-
-
-void setup() {
-  pinMode (trig, OUTPUT);
-  pinMode (echo, INPUT);
+void setup()
+{
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
   pinMode(S, INPUT_PULLUP);
 
-
   // led
-  pinMode(G1, OUTPUT);
-  pinMode(G2, OUTPUT);
-  pinMode(Y1, OUTPUT);
-  pinMode(Y2, OUTPUT);
-  pinMode(R1, OUTPUT);
-  pinMode(R2, OUTPUT);
-  
-  Serial.begin (9600);
+  pinMode(R, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(G, OUTPUT);
+
+  Serial.begin(9600);
 }
 
-void loop() {
-  if ( digitalRead(S) == LOW ) {
-    digitalWrite(G1, LOW);
-    digitalWrite(G2, LOW);
-    digitalWrite(Y1, LOW);
-    digitalWrite(Y2, LOW);
-    digitalWrite(R1, LOW);
-    digitalWrite(R2, LOW);
-   
-  } else {
+void loop()
+{
+  if (digitalRead(S) == LOW)
+  {
+    digitalWrite(R, LOW);
+    digitalWrite(B, LOW);
+    digitalWrite(G, LOW);
+  }
+  else
+  {
 
     digitalWrite(trig, LOW);
     delay(0005);
     digitalWrite(trig, HIGH);
     delay(0010);
     digitalWrite(trig, LOW);
-    
-    distancia = pulseIn (echo, HIGH);
-    distancia = distancia/58;
-    Serial.println (distancia);
-    
+
+    distancia = pulseIn(echo, HIGH);
+    distancia = distancia / 58;
+    Serial.println(distancia);
+
     int distancia2 = (int)distancia;
 
-    if (distancia > 120) {
-      digitalWrite(G1, LOW);
-      digitalWrite(G2, LOW);
-      digitalWrite(Y1, LOW);
-      digitalWrite(Y2, LOW);
-      digitalWrite(R1, LOW);
-      digitalWrite(R2, LOW);
-
-    } else if (distancia > 100) {
-      digitalWrite(G1, HIGH);
-      digitalWrite(G2, LOW);
-      digitalWrite(Y1, LOW);
-      digitalWrite(Y2, LOW);
-      digitalWrite(R1, LOW);
-      digitalWrite(R2, LOW);
-
-    } else if (distancia > 80) {
-      digitalWrite(G1, HIGH);
-      digitalWrite(G2, HIGH);
-      digitalWrite(Y1, LOW);
-      digitalWrite(Y2, LOW);
-      digitalWrite(R1, LOW);
-      digitalWrite(R2, LOW);
-    } else if (distancia > 70) {
-      digitalWrite(G1, HIGH);
-      digitalWrite(G2, HIGH);
-      digitalWrite(Y1, HIGH);
-      digitalWrite(Y2, LOW);
-      digitalWrite(R1, LOW);
-      digitalWrite(R2, LOW);
-    } else if (distancia > 60) {
-      digitalWrite(G1, HIGH);
-      digitalWrite(G2, HIGH);
-      digitalWrite(Y1, HIGH);
-      digitalWrite(Y2, HIGH);
-      digitalWrite(R1, LOW);
-      digitalWrite(R2, LOW);
-    } else if (distancia > 40) {
-      digitalWrite(G1, HIGH);
-      digitalWrite(G2, HIGH);
-      digitalWrite(Y1, HIGH);
-      digitalWrite(Y2, HIGH);
-      digitalWrite(R1, HIGH);
-      digitalWrite(R2, LOW);
-    } else {
-      digitalWrite(G1, HIGH);
-      digitalWrite(G2, HIGH);
-      digitalWrite(Y1, HIGH);
-      digitalWrite(Y2, HIGH);
-      digitalWrite(R1, HIGH);
-      digitalWrite(R2, HIGH);
+    if (distancia > 60)
+    {
+      analogWrite(R, 0);
+      analogWrite(G, 0);
+      analogWrite(B, 0);
+    }
+    else if (distancia > 100)
+    {
+      analogWrite(R, 90);
+      analogWrite(G, 60);
+      analogWrite(B, 30);
+    }
+    else if (distancia > 80)
+    {
+      analogWrite(R, 60);
+      analogWrite(G, 60);
+      analogWrite(B, 60);
+    }
+    else if (distancia > 70)
+    {
+      analogWrite(R, 30);
+      analogWrite(G, 60);
+      analogWrite(B, 90);
+    }
+    else if (distancia > 60)
+    {
+      analogWrite(R, 100);
+      analogWrite(G, 100);
+      analogWrite(B, 30);
+    }
+    else if (distancia > 40)
+    {
+      analogWrite(R, 255);
+      analogWrite(G, 10);
+      analogWrite(B, 150);
+    }
+    else
+    {
+      analogWrite(R, 255);
+      analogWrite(G, 255);
+      analogWrite(B, 255);
     }
   }
-  
-
 }
